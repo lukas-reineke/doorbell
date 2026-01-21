7a9c1091bd3e2bcdfac5f11938ba95df0c500f36
Lukas Reineke <lukas@reineke.jp>
2026-01-21 13:57:25 +0900

    don't play sound when in meeting

--- notification.py
+++ notification.py
@@ -4,10 +4,27 @@ import time
 
 import paho.mqtt.client as mqtt
 import pygame
+import requests
+
+try:
+    with open(".env") as f:
+        for line in f:
+            line = line.strip()
+            if not line or line.startswith("#"):
+                continue
+            key, value = line.split("=", 1)
+            os.environ.setdefault(key, value)
+except FileNotFoundError:
+    raise RuntimeError(".env not found")
 
 MQTT_BROKER = "192.168.3.4"
 MQTT_TOPIC = "home/doorbell/ring"
 SOUND_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "notification.mp3")
+CAMERA_FILE = os.path.join(os.path.dirname(os.path.realpath(__file__)), "camera.sh")
+HA_URL = os.environ.get("HA_URL")
+TOKEN = os.environ.get("HA_TOKEN")
+ENTITY_ID = "input_boolean.in_a_meeting"
+
 
 pygame.mixer.init()
 pygame.mixer.music.load(SOUND_FILE)
@@ -22,8 +39,25 @@ def stop_music():
     subprocess.run(["playerctl", "pause"])
 
 
+def open_camera():
+    subprocess.Popen(["bash", CAMERA_FILE])
+
+
 def play_sound():
     pygame.mixer.music.play()
+
+
+def in_meeting():
+    resp = requests.get(
+        f"{HA_URL}/api/states/{ENTITY_ID}",
+        headers={
+            "Authorization": f"Bearer {TOKEN}",
+            "Content-Type": "application/json",
+        },
+    )
+    resp.raise_for_status()
+
+    return resp.json()["state"] == "on"
 
 
 def on_connect(client, userdata, flags, rc):
@@ -42,9 +76,11 @@ def on_disconnect(client, userdata, rc):
 def on_message(client, userdata, msg):
     payload = msg.payload.decode()
     print(f"[MQTT] Received: {payload}")
-    stop_music()
-    play_sound()
     notify()
+    if not in_meeting():
+        open_camera()
+        stop_music()
+        play_sound()
 
 
 def reconnect(client):
